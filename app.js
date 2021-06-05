7b133148aa32df4c4f8a1040e7a92a4686cec754
Loris <45939615+lchastanet@users.noreply.github.com>
2021-06-05 20:11:15 +0200

    add auth system

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
 const stuffRoutes = require("./routes/stuff")
+const userRoutes = require("./routes/user")
 
 const app = express()
 
@@ -30,5 +31,6 @@ app.use((req, res, next) => {
 app.use(express.json())
 
 app.use("/api/stuff", stuffRoutes)
+app.use("/api/auth", userRoutes)
 
 module.exports = app
